742593e0cf5a5e785e29116aafbe300c6a07452c
Rahul Gupta <rahulg.wappnet@gmail.com>
2023-03-02 16:35:50 +0530

    user module created without cli

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,24 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get ,Post,Req,Param} from '@nestjs/common';
+import { Request } from 'express';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
-
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  getUsers(){
+    return{
+      name:"Rahul",
+      email:"rahulguptaslg20@gmail.com"
+    }
   }
+
+  @Post('/add')
+  storeUser(@Req() req:Request){
+      return req.body
+  }
+
+  // @Get('/:userid')
+  // getUser(@Param() params:{userId:Number}){
+  //   return params
+  // }
 }
